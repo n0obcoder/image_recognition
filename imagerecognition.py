4192e8a97c1dcd8291b7e533381ad427615755cb
n0obcoder <31921968+n0obcoder@users.noreply.github.com>
2017-10-06 00:59:23 +0530

    Add files via upload

---
+++ imagerecognition.py
@@ -0,0 +1,13 @@
+from PIL import Image
+import numpy as np
+####
+import matplotlib.pyplot as plt
+###
+i = Image.open('images/dotndot.png')
+
+iar = np.asarray(i)
+
+
+plt.imshow(iar)
+print(iar)
+plt.show()
